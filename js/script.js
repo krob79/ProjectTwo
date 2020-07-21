d8d1cbb4b5157a08ecad99f1307210c02c9947a2
krob79 <emailkyleroberts@gmail.com>
2020-07-21 16:00:00 -0400

    Fixed Filtering and Adjusted Dynamic Page Links

--- js/script.js
+++ js/script.js
@@ -2,34 +2,37 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+//limits results to display per page
 const maxStudentsPerPage = 10;
+//array that stores student objects
 const allStudents = [];
+//stores the indexes of the first element on each page - used to know when to stop displaying results
+let pageIndexes = [];
+let pagesNeeded = 0;
+let currentPage = 0;
+
+//grab the page element and its student-list child
+const pageElement = document.getElementsByClassName('page')[0];
+//establish global var for pageLinks
+let pageLinksElement = '';
+
+const studentListElement = document.getElementsByClassName('student-list')[0];
+//create new resultsMsg DIV element and insert it before the student-list element
+const resultsMsgElement = document.createElement("DIV");
+resultsMsgElement.innerHTML = "<strong>NO RESULTS FOUND</strong>";
+resultsMsgElement.style.color = 'red';
+pageElement.insertBefore(resultsMsgElement, studentListElement);
+
 const students_liElements = document.getElementsByClassName('student-item cf');
 const studentNames = document.getElementsByTagName('h3');//studentNames[0].textContent
 const studentAvatars = document.getElementsByClassName('avatar'); //studentAvatars[0].src
 const studentEmails = document.getElementsByClassName('email');//studentEmails[0].textContent
 const studentJoinDates = document.getElementsByClassName('date');//studentJoinDates[0].textContent
-let pageIndexes = [];
-let pagesNeeded = 0;
 
 const header = document.getElementsByClassName('page-header cf')[0];
 const searchBar = document.createElement('input');
-searchBar.addEventListener("onkeydown", filterList);
+searchBar.onkeyup = filterList;
 searchBar.className = "student-search";
 const searchButton = document.createElement('button');
 searchButton.textContent = "Search";
@@ -54,87 +57,101 @@ for(let i = 0; i < students_liElements.length; i++){
     allStudents.push(student);
 }
 
-//console.log("---students: " + allStudents[0].name);
-//console.log("---students: " + allStudents[0].avatar);
-//console.log("---students: " + allStudents[0].email);
-//console.log("---students: " + allStudents[0].joinDate);
-
 function organizePages(studentList, maxPerPage){
     let length = studentList.length;
     pagesNeeded = Math.ceil(length / maxPerPage);
-    appendPageLinks(maxPerPage);
+    appendPageLinks(studentList, maxPerPage);
 }
-organizePages(allStudents, maxStudentsPerPage);
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 function showPage(students, page){
+    //calls organizePages
+    organizePages(students, maxStudentsPerPage);
+    //the count for students visible should be set to 0 by default, but keep the 'none found' message hidden
+    let studentsVisible = 0;
+    resultsMsgElement.style.display = 'none';
+    currentPage = page;
     //show only students from the array passed in
     for(let i = 0; i < students.length; i++){
+        /*
+        if the current student's index is greater than or equal to the index referenced in the pageIndexes array OR 
+        the current student's index is less than the pageIndex + maxStudentsPerPage (which would be the first index of 
+        the next page), show the student. Otherwise, hide the student.
+        */
         if(students.indexOf(students[i]) >= pageIndexes[page] && students.indexOf(students[i]) < (pageIndexes[page]+maxStudentsPerPage)){
             students[i].show();
+            //count how many students are visible
+            studentsVisible++;
         }else{
             students[i].hide();
         }
     }
-    //highlight the button that corresponds with the page you're on
-    let linkList = document.querySelector('.pagination');
-    let links = linkList.querySelectorAll('li > a');
-    for(let i = 0; i < links.length; i++){
-        if(i == page){
-            links[i].style.border = "1px solid firebrick";
-        }else{
-            links[i].style.border = "1px solid #eaeaea";
+    if(studentsVisible > 0){
+        //highlight the button that corresponds with the page you're on
+        let linkList = document.querySelector('.pagination');
+        let links = linkList.querySelectorAll('li > a');
+        for(let i = 0; i < links.length; i++){
+            if(i == page){
+                links[i].style.border = "1px solid firebrick";
+            }else{
+                links[i].style.border = "";
+            }
         }
+    }else{
+        resultsMsgElement.style.display = 'block';
     }
-    console.log("---LINKS: " + links.length);
 }
 
 function filterList(){
-    console.log("FILTERING for " + searchBar.value + " length: " + searchBar.value.length);
-    let students = allStudents;
+    //get text from searchbar
     let query = searchBar.value;
-    for(let i = 0; i < students.length; i++){
-        if(students[i].name.startsWith(searchBar.value)){
-            students[i].show();
-        }else{
-            students[i].hide();
+    //start with clear array
+    let filteredStudentList = [];
+    if(query != ''){
+        let studentsVisible = 0;
+        let students = allStudents;
+        //compare searchbar text with each student's name
+        for(let i = 0; i < students.length; i++){
+            //if student's name starts with the same letters from the searchbar, show it, else hide it.
+            if(students[i].name.startsWith(searchBar.value)){
+                //students[i].show();
+                filteredStudentList.push(students[i]);
+                //count how many students are visible
+                studentsVisible++;
+            }else{
+                students[i].hide();
+                console.log("Skipping");
+            }
         }
+        showPage(filteredStudentList, 0);
+    }else{
+        //run showPage with default students list
+        showPage(allStudents, currentPage);
     }
 }
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-function appendPageLinks(maxPerPage){
-    //add page links after the page div's <ul> element
-    //create the unordered list for the links
-    let pageLinks = document.createElement('ul');
-    pageLinks.className = 'pagination';
+function appendPageLinks(studentList, maxPerPage){
+    
+    var linkCheck = document.getElementsByClassName('pagination');
+    if(linkCheck.length > 0){
+        console.log("---has previous links - removing " + linkCheck);
+        pageElement.removeChild(pageLinksElement);
+    }else{
+        console.log("---does NOT have previous links");
+    }
+    
+    pageLinksElement = document.createElement('ul');
+    pageLinksElement.className = 'pagination';
     let pageIndex = 0;
     for(let i=0; i < pagesNeeded; i++){
         //add current index to the pageIndexes array, then increment by max 
         pageIndexes.push(pageIndex);
         pageIndex += maxPerPage;
         let pageLink = document.createElement('li');
-        pageLink.innerHTML = `<a>Page ${i+1}</a>`;
-        pageLink.addEventListener("click", (e) =>{console.log("click " + i); showPage(allStudents, i);});
-        pageLinks.appendChild(pageLink);
+        pageLink.innerHTML = `<a>${i+1}</a>`;
+        pageLink.addEventListener("click", (e) =>{showPage(studentList, i);});
+        pageLinksElement.appendChild(pageLink);
     }
-    document.getElementsByClassName('student-list')[0].appendChild(pageLinks);
+    pageElement.appendChild(pageLinksElement);
 }
 
 showPage(allStudents, 0)
